a269d51c304684555cacbc8741ae9c24a606c158
Lydia Scadding <lydiascadding@gmail.com>
2021-04-12 09:06:30 +0100

    Added associations and validations for InventoryItems

--- app/models/inventory_item.rb
+++ app/models/inventory_item.rb
@@ -1,4 +1,7 @@
 class InventoryItem < ApplicationRecord
   belongs_to :user
   belongs_to :book
+
+  validates :read_status, inclusion: { in: %w[read to-read currently-reading] }
+  validates :format, inclusion: { in: %w[paperback hardback ebook] }
 end
